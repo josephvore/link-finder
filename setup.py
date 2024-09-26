3ab7656602d83001cc3e5a3df2391c01414bc7fd
josephvore <josephvore97@gmail.com>
2024-09-26 17:19:07 -0600

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='link-finder',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'hrequests==0.8.2',
