cbfbbf7d083e407b61b5bbab419d7af1fdf4ca87
josephvore <josephvore97@gmail.com>
2024-09-26 22:23:05 -0600

    v0.1.2 updated init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='link-finder',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'hrequests==0.8.2',
